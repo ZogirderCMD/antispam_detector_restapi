08abe4d528db4afa0850be9aa7070103868e9c54
Fronya <67414826+ZogirderCMD@users.noreply.github.com>
2026-04-23 17:14:07 +0700

    Изменения
    Изменён main.py
     - добавлена модель антиспама

--- spamdetector/main.py
+++ spamdetector/main.py
@@ -1,17 +1,53 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from flask import Flask, make_response
-import psycopg2, os, datetime, torch
+import psycopg2, os, datetime, re, torch, sys
 
 
 def log(text):
     try:
         was_txt = open("logs.txt", "r").read()
     except OSError:
-        open("logs.txt", "w").write("\n")
+        open("logs.txt", "w").write("")
         was_txt = open("logs.txt", "r").read()
     txt = f"[{datetime.datetime.now()}]: {text}"
     open("logs.txt", "w").write(f"{was_txt}{txt}\n")
     print(txt)
+
+log("Starting service...")
+
+try:
+    class SpamDetector:
+        model_name = 'RUSpam/spamNS_v1'
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1).to(device).eval()
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        is_spam = False
+
+        def __init__(self, text):
+            self.classify_message(text)
+
+        def clean_text(self, text):
+            text = re.sub(r'http\S+', '', text)
+            text = re.sub(r'[^А-Яа-я0-9 ]+', ' ', text)
+            text = text.lower().strip()
+            return text
+
+        def classify_message(self, message):
+            message = self.clean_text(message)
+            encoding = self.tokenizer(message, padding='max_length', truncation=True, max_length=128, return_tensors='pt')
+            input_ids = encoding['input_ids'].to(self.device)
+            attention_mask = encoding['attention_mask'].to(self.device)
+
+            with torch.no_grad():
+                outputs = self.model(input_ids, attention_mask=attention_mask).logits
+                pred = torch.sigmoid(outputs).cpu().numpy()[0][0]
+
+            self.is_spam = pred >= 0.5
+except:
+    log("Failed to load antispam agent")
+    sys.exit()
 
 try:
     con = psycopg2.connect(
@@ -23,13 +59,14 @@ try:
     )
 except:
     log("Failed connection to database")
+    sys.exit()
 
 cur = con.cursor()
 
-def query(a):
+def query(q):
     try:
-        cur.execute(a)
+        cur.execute(q)
         con.commit()
     except Exception as e:
-        log(f"Error in sql query, queried \" {a} \", got \" {e} \"")
+        log(f"Error in sql query, queried \" {q} \", got \" {e} \"")
         con.rollback()
--- spamdetector/schema.sql
+++ spamdetector/schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS requests_history (
     id SERIAL PRIMARY KEY,
     input_text TEXT,
     spam BOOL NOT NULL,
-    model_name VARCHAR(10),
+    model_name VARCHAR(20),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 )
