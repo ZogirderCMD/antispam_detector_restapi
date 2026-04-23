ca7a7123b401d06ab758bfe49a85f34196227cb9
Fronya <67414826+ZogirderCMD@users.noreply.github.com>
2026-04-23 21:41:53 +0700

    Добавления и изменения
    Созданы функции для обработки запросов
    Разработан сервер Flask

--- spamdetector/main.py
+++ spamdetector/main.py
@@ -1,5 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 import psycopg2, os, datetime, re, torch, sys
 
 
@@ -24,6 +24,7 @@ try:
         tokenizer = AutoTokenizer.from_pretrained(model_name)
 
         is_spam = False
+        pred:float=0.0
 
         def __init__(self, text):
             self.classify_message(text)
@@ -42,12 +43,58 @@ try:
 
             with torch.no_grad():
                 outputs = self.model(input_ids, attention_mask=attention_mask).logits
-                pred = torch.sigmoid(outputs).cpu().numpy()[0][0]
+                self.pred = torch.sigmoid(outputs).cpu().numpy()[0][0]
 
-            self.is_spam = pred >= 0.5
+            self.is_spam = self.pred >= 0.5
 except:
     log("Failed to load antispam agent")
     sys.exit()
+
+class HistoryRequest:
+    text:str=""
+    spam:bool=False
+    model_name:str=""
+    pred:float=0.0
+    
+    def req(self, text=None):
+        if not text is None: 
+            self.text = text
+            try:
+                sd = SpamDetector(text)
+                self.spam = sd.is_spam
+                self.model_name = sd.model_name
+                self.pred = sd.pred
+                query(f"INSERT INTO (input_text, spam, model_name, pred) requests_history VALUES ({self.text, self.spam, self.model_name, self.pred})")
+                log(f"User made request with text: \" {text} \", result: {"POSITIVE" if self.spam else "NEGATIVE"}, prdiction score: {self.pred}")
+                return make_response({"result": "POSITIVE" if self.spam else "NEGATIVE", "score": self.pred}, 200)
+            except Exception as e:
+                log(f"Error on model: {e}")
+        else:
+            log(f"User made request with empty text!")
+            return make_response({"result": "Text area is empty!"}, 403)
+
+def getHistory(typ=1, ids=None):
+    if typ == 1:
+        query("SELECT id, model_name, created_at FROM requests_history ORDER BY created_at DESC LIMIT 20")
+        res = []
+        for i in cur.fetchall():
+            res.append({"id": i[0],"model_name": i[1],"created_at": i[2]})
+        log(f"User get last 20 requests")
+        return make_response({"result": res}, 200)
+    if typ == 2:
+        if ids is None:
+            log(f"User made request with empty ID!")
+            return make_response({"result": "Request ID is empty!"}, 403)
+        else:
+            query(f"SELECT input_text, spam, pred FROM requests_history WHERE id={ids}")
+            try:
+                res = cur.fetchall()
+                log(f"User get request with ID: {ids}")
+                return make_response({"input_text":res[0], "result": "POSITIVE" if res[1] else "NEGATIVE", "score": res[2]}, 200)
+            except:
+                log(f"User made request with unknown ID: {ids}")
+                return make_response({"result": "Unknown request ID!"}, 403)
+
 
 try:
     con = psycopg2.connect(
@@ -70,3 +117,28 @@ def query(q):
     except Exception as e:
         log(f"Error in sql query, queried \" {q} \", got \" {e} \"")
         con.rollback()
+
+try:
+    query(open("schema.sql","r").read())
+except:
+    log("Failed to load schema")
+
+app = Flask(__name__)
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    return HistoryRequest().req(request.form.get('text', None))
+
+@app.route("/history", methods=["GET"])
+def history():
+    return getHistory()
+
+@app.route("/history/<int:ids>", methods=["GET"])
+def historys(ids):
+    return getHistory(2, ids)
+
+@app.route("/health", methods=["GET"])
+def health():
+    return make_response({"status": "ok"}, 200)
+
+app.route(port=8000)
